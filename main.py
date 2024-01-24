22657e1c8c23f554c852ea053807276f2b22581f
Kyle Truong <kyle.trng@gmail.com>
2024-01-24 13:40:34 -0500

    Revert "testing lazygit"
    This reverts commit 337b3400aed474d5289f4da19e740b5c5d65f51f.

---
+++ main.py
@@ -0,0 +1,26 @@
+import random, wordle
+
+def validateWord(word: str) -> int:
+    if len(word) == reqLen:
+          if guessWord in game.wordbank: return 0
+          else: return 1
+    else: return 2
+
+if __name__ == "__main__":
+    getLen = input("Word Length: ")
+    reqLen = int(getLen) if getLen.isnumeric() and int(getLen) >= 4 and int(getLen) <= 7 else random.randint(4, 7)
+    game = wordle.Wordle(reqLen)
+    count = 0
+    while count in range(0, reqLen+1):
+        print(f'Guesses remaining: {reqLen+1-count}')
+        guessWord = input(f'Enter a {reqLen} length word: ')
+        match validateWord(guessWord):
+            case 0:
+                count += 1
+                print(f'{game.solve(guessWord)}\nLetters used: {game.letterBank.keys()}')
+                if guessWord == game.chosenWord: break
+            case 1: print('Word not in wordbank')
+            case 2: print(f'Word must be a {reqLen} long word')            
+    print(game.chosenWord)
+    input("")
+    # test
